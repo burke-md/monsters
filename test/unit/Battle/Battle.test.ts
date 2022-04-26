a8cab81769a8c230517608f0a5c1f07e17428825
Michael Burke <michael.dburke56@gmail.com>
2022-04-25 21:33:08 -0400

    Implement test structure boiler plate.

---
+++ test/unit/Battle/Battle.test.ts
@@ -0,0 +1,8 @@
+export const shouldBattle = (): void => {
+  //   // to silent warning for duplicate definition of Transfer event
+  //   ethers.utils.Logger.setLogLevel(ethers.utils.Logger.levels.OFF);
+
+  context(`#battle`, async function () {
+    it(`should start here...`, async function () {});
+  });
+};
---
+++ test/unit/index.ts
@@ -0,0 +1,12 @@
+import { shouldBattle } from "./Battle/Battle.test";
+
+describe('Unit tests', async function () {
+  before(async function () {
+  });
+  describe('Battle', async function () {
+    beforeEach(async function () {
+      //load fixture and mocks
+    });
+    shouldBattle();
+  });
+});
