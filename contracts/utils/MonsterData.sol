87e3ec002a77782ad3b40930035efa36eb6ff9b3
MB <michael.dburke56@gmail.com>
2022-05-25 12:58:21 -0500

    Implement counters properly.

--- contracts/utils/MonsterData.sol
+++ contracts/utils/MonsterData.sol
@@ -4,6 +4,6 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract MonsterData {
-    
+    using Counters for Counters.Counter;    
     Counters.Counter internal _tokenIdCounter;
 }
