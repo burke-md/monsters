823d0b494ba791f02ac9fc38f4d675c71bec7952
Colin <colinmaisk@gmail.com>
2022-05-24 20:46:19 -0700

    Implemented chainlink VRF v2

--- contracts/utils/RandomNumberVRF.sol
+++ contracts/utils/RandomNumberVRF.sol
@@ -1,29 +1,57 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract RandomNumberVRF is VRFConsumerBase {
-    bytes32 internal keyHash;
-    uint256 internal fee;
-    uint256 public randomResult;
+interface IUnmintedMonsters {
+    function getLengthUnmintedMonsters() external view returns(uint);
+}
 
-    constructor()
-        VRFConsumerBase(
-        0x6168499c0cFfCaCD319c818142124B7A15E857ab, // VRF Coordinator
-        0x01BE23585060835E02B77ef475b0Cc51aA1e0709  // LINK Token
-        )
-    {
-        keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
-        fee = 0.1 * 10 ** 18; // 0.1 LINK (Varies by network)
+contract RandomNumberVRF is VRFConsumerBaseV2, Ownable {
+    VRFCoordinatorV2Interface COORDINATOR;
+
+    uint64 s_subscriptionId = 4941;
+    address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
+    address link = 0x01BE23585060835E02B77ef475b0Cc51aA1e0709;
+    bytes32 keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
+    uint32 callbackGasLimit = 100000;
+    uint16 requestConfirmations = 3;
+    uint32 numWords =  2;
+
+    uint256[] public s_randomWords;
+    uint256 public s_randomNumber;
+    uint256 public s_requestId;
+    address s_owner;
+
+    address unMintedMonsterAddr;
+
+    constructor(uint64 subscriptionId) VRFConsumerBaseV2(vrfCoordinator) {
+        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+        s_owner = msg.sender;
+        s_subscriptionId = subscriptionId;
     }
 
-    function getRandomNumber() public returns (bytes32 requestId) {
-        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
-        return requestRandomness(keyHash, fee);
+    function requestRandomWords() external onlyOwner {
+        // Will revert if subscription is not set and funded.
+        s_requestId = COORDINATOR.requestRandomWords(
+            keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+    }
+    
+    function fulfillRandomWords(
+        uint256, /* requestId */
+        uint256[] memory randomWords
+    ) internal override {
+        s_randomNumber = (randomWords[0] % IUnmintedMonsters(unMintedMonsterAddr).getLengthUnmintedMonsters()) + 1;
     }
 
-    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
-        randomResult = (randomness % 2) + 1;
+    function setUnmintedMonsterAddr(address _unMintedMonsterContract) external onlyOwner {
+       unMintedMonsterAddr = _unMintedMonsterContract;
     }
 }
