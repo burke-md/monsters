94a52b033def7b5400644dbd2a22ccd73ed27728
MB <michael.dburke56@gmail.com>
2022-05-27 09:12:32 -0500

    Resolve typo.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,13 +5,13 @@
  import "@nomiclabs/hardhat-waffle";
  import "@typechain/hardhat";
 
- const { alchemyApiKey, mnemonic } = require('./secrets.json');
+ const { alchemyApiKey, mnemonic } = require("./.secrets.json");
 
 
  module.exports = {
   solidity: "0.8.4",
 
-    netowkrs: {
+    networks: {
         rinkeby: {
             url: `https://eth-rinkeby.alchemyapi.io/v2/${alchemyApiKey}`,
             accounts: { mnemonic: mnemonic },
