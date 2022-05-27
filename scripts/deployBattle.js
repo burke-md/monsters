9257af64b4424351d68f5563c0ec04cab828cd4d
MB <michael.dburke56@gmail.com>
2022-05-27 09:03:36 -0500

    Implement Battle deploy script.

--- scripts/deployBattle.js
+++ scripts/deployBattle.js
@@ -1,1 +1,14 @@
+async function main () {
+    const Battle = await ethers.getContractFactory('Battle');
+    console.log('Deploying Battle...');
+    const battle = await Battle.deploy();
+    await battle.deployed();
+    console.log('Battle deployed to: ', battle.address);
+}
 
+main()
+    .then(() => process.exit(0))
+    .catch((err) => {
+        console.error(err);
+        process.exit(1);
+    });
